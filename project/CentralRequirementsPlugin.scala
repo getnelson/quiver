4174dc9bce66b396e3145d67b930f770d18ef1d3
Ross A. Baker <ross@rossabaker.com>
2026-06-23 19:46:43 -0400

    Convert developers to avoid duplicates in POM

--- project/CentralRequirementsPlugin.scala
+++ project/CentralRequirementsPlugin.scala
@@ -7,25 +7,32 @@ object CentralRequirementsPlugin extends AutoPlugin {
   override def trigger = allRequirements
 
   override lazy val projectSettings = Seq(
-    pomExtra in Global := {
-      <developers>
-        <developer>
-          <id>timperrett</id>
-          <name>Timothy Perrett</name>
-          <url>http://github.com/timperrett</url>
-        </developer>
-        <developer>
-          <id>runarorama</id>
-          <name>Runar Bjarnason</name>
-          <url>http://github.com/runarorama</url>
-        </developer>
-        <developer>
-          <id>stew</id>
-          <name>Stew O'Connor</name>
-          <url>http://github.com/stew</url>
-        </developer>
-      </developers>
-    },
+    developers := List(
+      Developer(
+        "timperett",
+        "Timothy Perrett",
+        "@timperett",
+        java.net.URI.create("https://github.com/timperrett").toURL(),
+      ),
+      Developer(
+        "runarorama",
+        "Runar Bjarnason",
+        "@runarorama",
+        java.net.URI.create("https://github.com/runarorama").toURL(),
+      ),
+      Developer(
+        "stew",
+        "Stew O'Connor",
+        "@stew",
+        java.net.URI.create("https://github.com/stew").toURL(),
+      ),
+      Developer(
+        "rossabaker",
+        "Ross A. Baker",
+        "@rossabaker",
+        java.net.URI.create("https://rossabaker.com/").toURL(),
+      ),
+    ),
     licenses := Seq(
       "Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")
     ),
